c82f8a89a36bb8b6f83aac86a94c05a7e867d224
Feras Bin Sheha <ferasbinsheha@gmail.com>
2026-06-20 17:17:35 +0000

    Add expense form

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+expenses = []
+
 @app.route("/")
 def home():
-    return "Cloud Expense Tracker is Running!"
+    html = """
+    <h1>Expense Tracker</h1>
+
+    <form action="/add" method="post">
+        <input name="name" placeholder="Expense Name">
+        <input name="amount" placeholder="Amount">
+        <button type="submit">Add Expense</button>
+    </form>
+
+    <h2>Expenses</h2>
+    """
+
+    for e in expenses:
+        html += f"<p>{e['name']} - ${e['amount']}</p>"
+
+    return html
+
+@app.route("/add", methods=["POST"])
+def add():
+    expenses.append({
+        "name": request.form["name"],
+        "amount": request.form["amount"]
+    })
+    return home()
